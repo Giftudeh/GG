211d2920a0fea0c88c43eb90d3ecc023e2696043
Giftamuche <157594539+Giftamuche@users.noreply.github.com>
2024-04-19 13:08:50 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+class car{
+    constructor(carName,carColor, carModel, carType){
+        this.name = carName;
+        this.color = carColor;
+        this.model = carModel;
+        this.type = carType;
+
+    }
+    getCarName(){
+        return this.name;
+    }
+}
+
+function addDivide (a,b){
+    return(a + b) /2
+}
